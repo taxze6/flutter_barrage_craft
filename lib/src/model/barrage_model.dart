b44a2bf3427ef5ec42c2b2ee3462ba6d505aa593
Taxze <1929509811@qq.com>
2024-02-22 21:37:33 +0800

    🏹Complete the barrage model class

--- lib/src/model/barrage_model.dart
+++ lib/src/model/barrage_model.dart
@@ -1,3 +1,50 @@
-class BarrageModel {
+import 'package:flutter/cupertino.dart';
+import '../config/barrage_config.dart';
 
+class BarrageModel {
+  UniqueKey barrageId;
+  double offsetY;
+  double runDistance = 0;
+  double everyFrameRunDistance;
+  Size barrageSize;
+
+  BarrageModel({
+    required this.barrageId,
+    required this.offsetY,
+    required this.everyFrameRunDistance,
+    required this.runDistance,
+    required this.barrageSize,
+    //offset in milliseconds
+    required int offsetMS,
+  }) {
+    runDistance = (offsetMS / BarrageConfig.unitTimer) * everyFrameRunDistance;
+  }
+
+  /// The X-axis position of the barrage.
+  double get offsetX => runDistance - barrageSize.width;
+
+  /// Maximum run distance of barrage Barrage width + wall width.
+  double get maxRunDistance => barrageSize.width + BarrageConfig.areaSize.width;
+
+  /// The barrage is completely detached from the right wall.
+  bool get allOutRight => runDistance > barrageSize.width;
+
+  /// The whole screen is off the screen.
+  bool get allOutLeave => runDistance > maxRunDistance;
+
+  /// Remaining distance away.
+  double get remainderDistance => needRunDistance - runDistance;
+
+  /// Distance to travel.
+  double get needRunDistance =>
+      BarrageConfig.areaSize.width + barrageSize.width;
+
+  /// The time remaining away from the screen.
+  double get leaveScreenRemainderTime =>
+      remainderDistance / everyFrameRunDistance;
+
+  /// The barrage executes the next frame.
+  void runNextFrame() {
+    runDistance += everyFrameRunDistance * BarrageConfig.barrageRate;
+  }
 }
