93975e4459fccaee641337af45f0eca7d47dbeb7
徐臻杰 <1929509811@qq.com>
2024-03-02 20:47:41 +0800

    🎨Export file.

--- lib/flutter_barrage_craft.dart
+++ lib/flutter_barrage_craft.dart
@@ -1,3 +1,4 @@
-class FlutterBarrageCraft {
+library flutter_barrage_craft;
 
-}
+export 'package:flutter_barrage_craft/src/barrage_view.dart';
+export 'package:flutter_barrage_craft/src/barrage_controller.dart';
